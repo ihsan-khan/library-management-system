b8cbcc269db64940e5ffd9f5d632caddd79493dd
ihsan-khan <ehsankhan0577@gmail.com>
2025-10-30 16:33:40 +0500

    model book created

--- library/models.py
+++ library/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=13, unique=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    publisher = models.CharField(max_length=100)
+    published_date = models.DateField()
+    total_copies = models.PositiveIntegerField()
+    available_copies = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
