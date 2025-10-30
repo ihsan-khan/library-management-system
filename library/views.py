ab1a292f6f5ec4832d45c2b410d9529ae51e92f7
ihsan-khan <ehsankhan0577@gmail.com>
2025-10-30 16:27:18 +0500

    view templates

--- library/views.py
+++ library/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Welcome to the Library Management System")
+def index(request):
+    return render(request, 'library/index.html')
