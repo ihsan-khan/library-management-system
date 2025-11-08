ce7e8bea53794e84bce7c238fd0aa690a2c06c07
ihsan-khan <ehsankhan0577@gmail.com>
2025-11-08 13:21:51 +0500

    admin portal customization

--- LibraryManagement/urls.py
+++ LibraryManagement/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 admin.site.site_header = "Library Management System Admin"
+admin.site.index_title = "Admin Portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
