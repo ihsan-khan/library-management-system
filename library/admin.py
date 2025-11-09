6c393171bc1ab04e178513f146f728411b760023
ihsan-khan <ehsankhan0577@gmail.com>
2025-11-09 12:45:48 +0500

    all models were added

--- library/admin.py
+++ library/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 admin.site.register(models.Book)
+admin.site.register(models.Author)
+admin.site.register(models.Loan)    
+admin.site.register(models.Category)
+admin.site.register(models.Member)
+admin.site.register(models.Fine)
+
